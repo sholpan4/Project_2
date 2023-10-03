fe82fb317fc993e29ac19a062e4ce231d3d54db6
ramazansartayev <83175478+Ramazan-maker@users.noreply.github.com>
2023-10-03 18:44:22 +0600

    update

---
+++ gui.py
@@ -0,0 +1,49 @@
+from logger import log
+from PyQt6.QtCore import QObject
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtWidgets import QWidget
+from .main_window import MainWindow
+from .login_window import LoginWindow
+
+
+class Gui(QObject):
+    sendMessage = pyqtSignal(str, str)
+    loginUser = pyqtSignal(str)
+    window: QWidget = None
+    show_message = pyqtSignal(str)
+    change_chat = pyqtSignal(str)
+    def __init__(self):
+        super().__init__()
+        self.running = False
+        self.set_window('LoginWindow')
+
+    def start(self):
+        self.run()
+
+    def run(self):
+        log.i("Интерфэйс запущен")
+        self.window.show()
+        self.running = True
+
+    def set_window(self, window_name, username=None):
+        if self.window is not None:
+            self.window.hide()
+        match window_name:
+            case 'MainWindow':
+                self.window = MainWindow(username)
+                self.show_message.connect(self.window.show_message)
+                self.window.sendMessage.connect(self.sendMessage)
+            case 'LoginWindow':
+                self.window = LoginWindow()
+                self.window.loginUser.connect(self.loginUser)
+            case _:
+                log.e('Неизвестное имя окна:', window_name)
+        if self.running:
+            self.run()
+
+    def add_contact(self, name_contact):
+        pass
+
+    def set_chat(self, name_chat):
+        pass
+  
---
+++ router.py
@@ -0,0 +1,54 @@
+from PyQt6.QtCore import QObject
+from logger import log
+from gui import Gui
+from data_storage import DataStorage
+from udp_receiver import UdpReceiver
+from udp_sender import UdpSender
+from controller import Controller
+
+
+class Router(QObject):
+    def __init__(self):
+        super().__init__()
+        self.data_storage = DataStorage()
+        self.gui = Gui()
+        self.udp_receiver = UdpReceiver()
+        self.udp_sender = UdpSender()
+        self.controller = Controller()
+        # Здесь будем роутить сигналы
+
+        # Сигналы Gui
+        self.gui.loginUser.connect(self.data_storage.auth)
+        self.gui.loginUser.connect(self.controller.login)
+        self.gui.sendMessage.connect(self.controller.send)
+        self.gui.changeChat.connect(self.controller.change_chat)
+
+        # Сигналы Контроллера
+        self.controller.switchWindow.connect(self.gui.set_window)
+        self.controller.addContact.connect(self.gui.add_contact)
+        self.controller.showMessage.connect(self.gui.show_message)
+        self.controller.sendMessage.connect(self.udp_sender.send)
+        self.controller.setChat.connect(self.gui.set_chat)
+
+        # Сигналы UdpReceiver
+        self.udp_receiver.message.connect(self.controller.recived_message)
+        
+        self.udp_receiver.hello.connect(self.controller.recived_hello)
+
+        # Сигналы DataStorage
+        self.data_storage.ready.connect(self.controller.database_ready)
+        self.data_storage.authOk.connect(self.controller.database_auth_ok)
+        self.data_storage.authBad.connect(self.controller.database_auth_bad)
+    
+    def start(self):
+        log.i("Стартуем роутер")
+        self.data_storage.start()
+        self.gui.start()
+        self.udp_receiver.start()
+        self.udp_sender.start()
+
+    def stop(self):
+        self.udp_sender.stop()
+        self.udp_receiver.stop()
+        self.gui.stop()
+        self.data_storage.stop()

5a388fa4737b227a6a49137d9e32ea184b8608d5
S <52811487+sholpan4@users.noreply.github.com>
2023-10-03 18:25:32 +0600

    Update data.py
    '

--- NetChat/data_storage/data.py
+++ NetChat/data_storage/data.py
@@ -4,9 +4,15 @@ from logger import log
 
 class DataStorage(QThread):
     username = None
+    password = None
     
     def run(self):
         log.i("Data storage is running")
         
     def auth(self, username):
         self.username = username
+        
+    def login(self), username, password):
+        if self.username == username:
+            if self.password == password:
+                pass

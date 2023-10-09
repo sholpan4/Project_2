3d984518c00a183c44eec4af88227017f9bbf487
ramazansartayev <83175478+Ramazan-maker@users.noreply.github.com>
2023-10-09 21:39:16 +0600

    update

--- NetChat/udp_sender/udp_sender.py
+++ NetChat/udp_sender/udp_sender.py
@@ -1,28 +1,28 @@
-import socket
 from PyQt6.QtCore import QThread, pyqtSignal
-from logger import log
+import socket
 import time
 import datetime
-import threading #для установки lock 
+from logger import log
+import threading
 from message import Message
 
 
 class UdpSender(QThread):
     _queue = []
-    send = pyqtSignal(Message)
-    
+    sent = pyqtSignal(Message)
+
+
     def __init__(self):
         super().__init__()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.address = ('localhost', 9900)
-        self.runnning = False
+        self.addres = ('localhost', 9900)
+        self.running = False
         self.lock = threading.Lock()
-   
+
     def run(self):
-        log.i('Sender is runnign')
+        log.i("Сэндер запущен")
         self.running = True
         msg : Message = None
-
         while self.running:
             if len(self._queue) > 0:
                 self.lock.acquire()
@@ -30,15 +30,17 @@ class UdpSender(QThread):
                 self.lock.release()
                 msg.time = datetime.datetime.now().strftime('%H:%M:%S')
                 string_to_send = msg.toJson()
-                self.socket.sendto(string_to_send.encode(), self.address)
+                self.socket.sendto(string_to_send.encode(), self.addres)
                 self.sent.emit(msg)
             else:
                 time.sleep(0.025)
+
+
     def send(self, msg : Message):
         self.lock.acquire()
         self._queue.append(msg)
         self.lock.release()
-        
+
     def stop(self):
         self.running = False
         super().stop()
